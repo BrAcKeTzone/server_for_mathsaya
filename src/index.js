6561ecf2eed00cc7b9da653e44bd691c2c1caf7b
JhulPona <jpskiemaniwang@gmail.com>
2024-01-18 13:20:50 +0800

    alter the question public id to null

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ app.use("/questions", questionsRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   sequelize
-    .sync({ alter: true }) // Use { force: true } during development to drop and recreate tables
+    .sync({ alter: false }) // Use { force: true } during development to drop and recreate tables
     .then(() => {
       console.log("Connected to the database");
     })
