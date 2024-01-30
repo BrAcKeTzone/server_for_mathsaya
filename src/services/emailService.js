8f2c97c0ecd00d217dd0d11c579024a79cf03026
JhulPona <jpskiemaniwang@gmail.com>
2024-01-30 14:04:21 +0800

    edit the format on OTP message

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -17,19 +17,13 @@ const sendOTPEmail = async (email, otp) => {
     html: `
     <html>
       <body style="font-family: Arial, sans-serif; padding: 20px;">
-
         <h2>Welcome to MathSaya, Teacher!</h2>
-
         <p>Thank you for choosing MathSaya! Your One-Time Passcode (OTP) is:</p>
-
         <h3 style="background-color: #f5f5f5; padding: 10px; border-radius: 5px;">${otp}</h3>
-
         <p>This OTP is only valid for 5 minutes, so please use it promptly to complete your action.</p>
-
         <p>If you have any questions or need assistance, feel free to contact our support team.</p>
-
         <p>Best regards,<br/>The MathSaya Team</p>
-
+        <p style="color: #888; font-size: 12px;background-color: #f5f5f5;padding:5px; text-align: center">This is a system-generated email. Please do not reply.</p>
       </body>
     </html>
   `,
