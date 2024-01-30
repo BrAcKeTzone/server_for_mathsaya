e4d10a673831aa15e076dcc9fdd46f137ae0b608
JhulPona <jpskiemaniwang@gmail.com>
2024-01-30 12:57:09 +0800

    record every student login

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ app.use("/questions", questionsRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   sequelize
-    .sync({ alter: true }) // Use { force: true } or { alter: true } during development to drop and recreate tables
+    .sync({ alter: false }) // Use { force: true } or { alter: true } during development to drop and recreate tables
     .then(() => {
       console.log("Connected to the database");
     })
